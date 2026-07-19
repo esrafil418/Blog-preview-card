4b3cae51301560f2fb4a6a2623c0cc08309c363a
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-19 09:03:35 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Blog preview card solution
+
+This is my solution to the [Blog preview card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS). This challenge helped me practice HTML, CSS, and Tailwind CSS by building a card component.
+
+---
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the card with a blog preview
+- See hover and focus states for interactive elements (the title changes color)
+
+### Screenshot
+
+![Screenshot](public/Screenshot%202026-07-19%20085853.png)
+
+### Links
+
+- Solution URL: []()
+- Live Site URL: []()
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Vite](https://vitejs.dev/) - Build tool
+- [Tailwind CSS v4](https://tailwindcss.com/) - For styling
+- Custom fonts (Figtree)
+
+### What I learned
+
+In this project, I learned how to:
+
+1. **Use Tailwind CSS v4** - I used the new `@theme` rule to add custom fonts.
+
+```css
+@theme {
+  --font-figtree: "Figtree", sans-serif;
+}
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+export default function App() {
+  return (
+    <div className='font-figtree flex items-center justify-center bg-[#f6d050] min-h-screen'>
+      <div className="flex flex-col gap-4 bg-[#ffffff] items-center justify-center px-5 py-6 rounded-xl max-w-sm border border-black shadow-[8px_8px_0px_0px_#000000] mx-5">
+        <div className="">
+          <img src="src/assets/images/illustration-article.svg" alt="illustration-article" className="rounded-xl h-full overflow-hidden"/>
+        </div>
+      <div className="flex flex-col gap-5">
+          <div className="flex flex-col items-start gap-2">
+          <p className="text-sm font-bold bg-[#f6d050] rounded-md px-3 py-1 inline-block">Learning</p>
+          <p className="text-sm text-black/80">Published 21 Dec 2023</p>
+        </div>
+        <div className="">
+          <p className="text-2xl font-extrabold hover:text-[#f6d050] cursor-pointer transition-colors duration-300">HTML & CSS foundations</p>
+          <p className="text-gray-500 leading-relaxed max-w-xs">These languages are the backbone of every website, defining structure, content, and presentation.</p>
+        </div>
+        <div className="flex items-center justify-baseline gap-3 font-bold">
+          <img src="src/assets/images/image-avatar.webp" alt="avatar" className="w-8 h-8 rounded-full cursor-pointer"/>
+          <p className="hover:text-[#dab747] cursor-pointer transition-colors duration-300">Greg Hooper</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
